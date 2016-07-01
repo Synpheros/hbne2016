0f56a9b43c0460d1a877fb36a07d3be12d6604d2
Iván J. Pérez <synpheros@hotmail.com>
2016-07-01 19:02:55 +0200

    [Scripts] Wavy Effect

---
+++ Assets/__Scripts/_WavyEffect.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _WavyEffect : MonoBehaviour {
+
+    public enum movingState
+    {
+        ENSANCHANDO_BAJITO,
+        VOLVIENDO_DE_BAJITO,
+        INCREMENTANDO_ALTO,
+        VOLVIENDO_DE_ALTO
+    }
+
+    float frecuency = 1f;
+    movingState state = movingState.ENSANCHANDO_BAJITO;
+
+    float height_percent = 1f;
+    float width_percent = 1f;
+    Vector3 pos;
+
+    float width, height, realheight;
+	void Start () {
+        this.width = this.transform.localScale.x;
+        this.height = this.transform.localScale.y;
+        
+        this.realheight = this.GetComponent<RectTransform>().sizeDelta.y;
+        move(this.transform.localPosition);
+    }
+
+    public void move(Vector3 position)
+    {
+        this.pos = position - new Vector3(0, (this.realheight * this.transform.localScale.y) / 2, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        switch (state)
+        {
+            case movingState.ENSANCHANDO_BAJITO:
+                this.height_percent -= 0.01f;
+                this.width_percent += 0.01f;
+
+                
+                if (this.height_percent <= 0.9)
+                    this.state = movingState.VOLVIENDO_DE_BAJITO;
+                break;
+            case movingState.VOLVIENDO_DE_BAJITO:
+                this.height_percent += 0.01f;
+                this.width_percent -= 0.01f;
+                if (this.height_percent >= 1.1f)
+                    this.state = movingState.ENSANCHANDO_BAJITO;
+                break;
+        }
+
+        this.transform.localScale = Vector3.Lerp(this.transform.localScale,new Vector3(this.width * width_percent, this.height * height_percent, 1f),0.1f);
+        this.transform.localPosition = new Vector3(this.pos.x, this.pos.y + (this.realheight*this.transform.localScale.y) / 2, this.pos.z);
+    }
+}
