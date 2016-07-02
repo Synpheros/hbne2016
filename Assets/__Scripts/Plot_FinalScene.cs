22f74dd7ea584faf23de26073b426b88e55fe2a9
Iván J. Pérez <synpheros@hotmail.com>
2016-07-02 20:24:33 +0200

    [Final Day] Escena del beso.

---
+++ Assets/__Scripts/Plot_FinalScene.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Plot_FinalScene : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject player = GameObject.Find("Player");
+        GameObject director = GameObject.Find("Director");
+
+        List<SequenceNode> sequence = new List<SequenceNode>();
+
+        sequence.Add(new SequenceNode("Tú: Madre mía... Al fín ha llegado el gran día.", new int[] { -1, -1, 0 }));
+        sequence.Add(new SequenceNode("Tú: Hoy es mi actuación como bailarín principal.", new int[] { -1, -1, 0 }));
+        sequence.Add(new SequenceNode("Alicia: Me alegro tanto, te lo has ganado con tu esfuerzo.", new int[] { 1, -1, -1 }));
+        sequence.Add(new SequenceNode("Tú: ¿Tú crees?", new int[] { -1, -1, 0 }));
+        sequence.Add(new SequenceNode("Alicia: Por supuesto. Eres el mejor bailarín que conozco.", new int[] { 1, -1, -1 }));
+        sequence.Add(new SequenceNode("Tú: Sabes...", new int[] { 1, -1, 0 }));
+        sequence.Add(new SequenceNode("Tú: Nada de esto habría sido posible...", new int[] { 1, -1, 0 }));
+        sequence.Add(new SequenceNode("Tú: Sin ti.", new int[] { 1, -1, 0 }));
+        sequence.Add(new SequenceNode("...", new int[] { 1, -1, 0 }));
+        sequence.Add(new SequenceNode("...", new int[] { -1, 1, 0 }));
+        sequence.Add(new SequenceNode("...", new int[] { -1, -1, 3 }));
+
+        SequenceManager.S.current_sequence = sequence;
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
